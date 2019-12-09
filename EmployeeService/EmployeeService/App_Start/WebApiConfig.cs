77d264dd035ac30d3aa7fd9a4512baeba3a5241c
manoj kumar <kumar.manoj@thinksys.com>
2019-12-09 19:25:18 +0530

    Firts Push

---
+++ EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace EmployeeService
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess1;
+
+namespace EmployeeService.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> Get() {
+
+            using(EmployeeDBEntities enitites=new EmployeeDBEntities())
+            {
+                return enitites.Employees.ToList();
+            }
+
+        }
+
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities enitites = new EmployeeDBEntities())
+            {
+                return enitites.Employees.FirstOrDefault(e=>e.ID==id);
+            }
+        }
+    }
+}
