db406d69d8e5406327213796da559befc266fadb
manoj kumar <kumar.manoj@thinksys.com>
2019-12-10 19:34:30 +0530

    Using HttpResponseMessage

--- EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
+++ EmployeeService/EmployeeService/App_Start/WebApiConfig.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace EmployeeService
 {
+    public class CustomJsonFormatter: JsonMediaTypeFormatter
+    {
+       public CustomJsonFormatter()
+        {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("apploication/json");
+        }
+    }
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
@@ -21,7 +36,7 @@ namespace EmployeeService
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
-
+            config.Formatters.Add(new CustomJsonFormatter());
             //config.Formatters.Remove(config.Formatters.XmlFormatter);
             //config.Formatters.Remove(config.Formatters.JsonFormatter);
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
--- EmployeeService/EmployeeService/Controllers/EmployeeController.cs
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -19,11 +19,40 @@ namespace EmployeeService.Controllers
 
         }
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
             using (EmployeeDBEntities enitites = new EmployeeDBEntities())
             {
-                return enitites.Employees.FirstOrDefault(e=>e.ID==id);
+                var en= enitites.Employees.FirstOrDefault(e=>e.ID==id);
+                if (en != null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.Found, en);
+                }
+                else
+                {
+                   return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Data with id=" + id + " " + "not found");
+                }
+            }
+        }
+
+        public HttpResponseMessage Post([FromBody]Employee employee)
+        {
+           try
+            {
+                using (EmployeeDBEntities enitites = new EmployeeDBEntities())
+                {
+                    enitites.Employees.Add(employee);
+                    enitites.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+
+                }
+            }
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
     }
