e9de6ae54cffab3a3c7557b362240bf4e5d0c19b
manoj kumar <kumar.manoj@thinksys.com>
2020-01-13 19:54:00 +0530

    Google authentication

--- EmployeeService/EmployeeService/Controllers/EmployeeController.cs
+++ EmployeeService/EmployeeService/Controllers/EmployeeController.cs
@@ -4,127 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using EmployeeDataAccess1;
 
 namespace EmployeeService.Controllers
 {
     public class EmployeeController : ApiController
-    {
-        [HttpGet]
-        public HttpResponseMessage LoadEmployeeData(string gender="ALL") {
-
-            using(EmployeeDBEntities enitites=new EmployeeDBEntities())
+    {   
+        [Authorize]
+        public IEnumerable<Employee> Get() {
+            using(EmployeeDBEntities enities=new EmployeeDBEntities())
             {
-                switch (gender.ToLower())
-                {
-                    case "all":
-                        return Request.CreateResponse(HttpStatusCode.OK, enitites.Employees.ToList());
-                    case "male":
-                        return Request.CreateResponse(HttpStatusCode.OK, enitites.Employees.Where(e=>e.Gender.ToLower()== "male").ToList());
-                    case "female":
-                        return Request.CreateResponse(HttpStatusCode.OK, enitites.Employees.Where(e => e.Gender.ToLower() == "female").ToList());
-                    default: return Request.CreateErrorResponse(HttpStatusCode.BadRequest, $"Value for gender must be ALL, Female and Male your query string for gender is ={gender} ");
-
-                }
+                return enities.Employees.ToList();
             }
 
-        }
-        [HttpGet]
-        public HttpResponseMessage LoadEmployeeByID(int id)
-        {
-            using (EmployeeDBEntities enitites = new EmployeeDBEntities())
-            {
-                var en= enitites.Employees.FirstOrDefault(e=>e.ID==id);
-                if (en != null)
-                {
-                    return Request.CreateResponse(HttpStatusCode.Found, en);
-                }
-                else
-                {
-                   return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Data with id=" + id + " " + "not found");
-                }
-            }
-        }
-
-        public HttpResponseMessage Post([FromBody]Employee employee)
-        {
-           try
-            {
-                using (EmployeeDBEntities enitites = new EmployeeDBEntities())
-                {
-                    enitites.Employees.Add(employee);
-                    enitites.SaveChanges();
-
-                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
-                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
-                    return message;
-
-                }
-            }
-            catch(Exception ex)
-            {
-                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
-            }
-        }
-
-        public HttpResponseMessage Delete(int id)
-        {
-            using (EmployeeDBEntities enitites = new EmployeeDBEntities())
-            {
-                try
-                {
-                    var en = enitites.Employees.FirstOrDefault(e => e.ID == id);
-                    if (en == null)
-                    {
-                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"record with id: {id} not found");
-                    }
-                    else
-                    {
-                        enitites.Employees.Remove(en);
-                        enitites.SaveChanges();
-                        return Request.CreateResponse(HttpStatusCode.OK);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
-
-                         
-                }
-               
-               
-            }
-        }
-
-        public HttpResponseMessage Put([FromBody]int id, [FromUri]Employee employee)
-        {
-            try
-            {
-                using (EmployeeDBEntities entities = new EmployeeDBEntities())
-                {
-                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
-
-                    if (entity == null)
-                        return Request.CreateErrorResponse(HttpStatusCode.NotFound, $"Employee with ID={id} not found");
-                    else
-                    {
-                        entity.FirstName = employee.FirstName;
-                        entity.LastName = employee.LastName;
-                        entity.Gender = employee.Gender;
-                        entity.Salary = employee.Salary;
-
-                        entities.SaveChanges();
-
-                        return Request.CreateResponse(HttpStatusCode.OK, entity);
-                    }
-
-                }
-            }
-            catch(Exception ex)
-            {
-               return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
-            }
-           
         }
     }
 }
---
+++ EmployeeService/GooogleAuthetication/Scripts/GoogleAuthetication.js
@@ -0,0 +1,48 @@
+﻿/// <reference path='jquery-1.10.2.js' />
+
+function getAccessToken() {
+    if (location.hash) {
+        if (location.hash.split('access_token=')) {
+            var accesToken = location.hash.split('access_token=')[1].split('&')[0];
+            if (accesToken)
+                IsUserRegistered(accesToken);
+        }
+    }
+}
+
+function IsUserRegistered(accesToken) {
+    $.ajax({
+        url: 'api/Account/UserInfo',
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accesToken
+        },
+        success: function (response) {
+            if (response.HasRegistered) {
+                localStorage.setItem('accessToken', accesToken);
+                localStorage.setItem('userName', response.Email);
+                window.location.href = 'Data.html';
+            }
+            else {
+                SignupExternally(accesToken)
+            }
+        }
+
+    });
+}
+
+function SignupExternally(accesToken) {
+    $.ajax({
+        url: 'api/Account/RegisterExternal',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + accesToken
+        },
+        success: function (response) {
+            window.location.href = "/api/Account/ExternalLogin?provider=Google&response_type=token&client_id=self&redirect_uri=http%3A%2F%2Flocalhost%3A61748%2FLogin.html&state=e-mLmpT0kW9HyicmWb1RsEpcxDxdMXYbGlp-Df9-dts1";
+        }
+
+    });
+}
